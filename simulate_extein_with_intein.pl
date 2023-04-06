070c3f6ec9037a73c0a8b3b347ee7b5dfdb866fd
sophia.gosselin <sophia.gosselin@uconn.edu>
2023-04-06 13:58:16 -0400

    v0.0.1

--- simulate_extein_with_intein.pl
+++ simulate_extein_with_intein.pl
@@ -5,28 +5,54 @@ use File::Copy;
 
 #USAGE: species_num tree_num birth_rate death_rate sampling_fraction mutation_rate
 
+#GLOBALS
 my $random_seed = int rand(1000000000);
 my($species_number,$tree_number,$birth_rate,$death_rate,$sample_fraction,$mutation_rate);
 GetOptions ('sp=s' => \$species_number, 'tn=s' =>\$tree_number, 'b=s' =>\$birth_rate, 'd=s' => \$death_rate, 'sf=s' => \$sample_fraction, 'm=s' => \$mutation_rate);
 
-SETUP();
+#Dicitonary for checking input parameters
+my %inputs = ("species_number" => $species_number,
+              "tree_number" => $tree_number,
+              "birth_rate" => $birth_rate,
+              "death_rate" => $death_rate,
+              "sample_fraction" => $sample_fraction,
+              "muation_rate" => $mutation_rate);
+
+#Code Start
+SETUP(\%inputs);
+MAIN();
 
 sub SETUP{
+  #check input variables
+  my $hash_ref = shift;
+  my %inputs_to_check = %{$hash_ref};
+  foreach my $input (keys %inputs_to_check){
+    if(!defined $inputs_to_check{$input}){
+      die "$input is not defined. Please check your inputs!\n";
+    }
+  }
+}
+
+sub MAIN{
+  my $extein_evolver_tree = EXTEIN_TREE();
+  die;
+}
+
+
+sub EXTEIN_TREE{
+  #make the extein tree
+  system("paml-evolver");
+  system("1"); #uses random unrooted tree option
+  system("$species_number"); #species number in tree
+  system("$tree_number\ $random_seed"); #number of trees and random seed
+  system("1"); #includes branch length from birth-death process
+  system("$birth_rate\ $death_rate\ $sample_fraction\ $mutation_rate"); #see variable name
+  #output will be evolver.out
   
 }
 
 
-#make the extein tree
-system("paml-evolver");
-system("1"); #uses random unrooted tree option
-system("$species_number"); #species number in tree
-system("$tree_number\ $random_seed"); #number of trees and random seed
-system("1"); #includes branch length from birth-death process
-system("$birth_rate\ $death_rate\ $sample_fraction\ $mutation_rate"); #see variable name
-
-#output will be evolver.out
-die;
-
+sub old_code{
 my $counter=0;
 open(IN, "< $ARGV[0]");
 my $tree_string;
@@ -70,4 +96,5 @@ L K M F P S T W Y V\n
   system("paml-evolver 7");
   chdir("..");
   $counter++;
+}
 }
