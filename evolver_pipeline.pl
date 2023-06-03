562f3fbacc42cb0221d8bbd33a07b0190467776d
Sophia.Gosselin <sophia.gosselin@uconn.edu>
2023-06-02 20:04:43 -0400

    Continuing overhaul. Still incomplete

--- evolver_pipeline.pl
+++ evolver_pipeline.pl
@@ -35,32 +35,42 @@ Example: sp 50 b 1 d 1 sf .1 m .01\n\n";
 MAIN();
 
 sub MAIN{
-  #starts by creating the extein tree.
-  my $user_check = "N";
-  my $tree_type = "extein";
-  my $input_primer_append = "Please input parameters for the $tree_type phylogeny now!\n".$input_primer;
-  %inputs = parse_and_check_inputs($input_primer_append);
-  #%inputs applies to the specific run. Therefore critical to keep seperate
-  #begin parameter testing for extein phylogeny
-  print "Beginning extein simulation parameters test.\n Testing will conclude once the user is happy with the tree and set parameters.\n"
-  tree_simulation_parameter_testing($user_check,$tree_type);
-  #save parameters settled on
-  my %extein_inputs =  %inputs;
-  #now evolves the intein tree
-  #gets new parameters first
-  $tree_type = "intein";
-  $input_primer_append = "Please input parameters for the $tree_type phylogeny now!\n".$input_primer;
-  %inputs = parse_and_check_inputs($input_primer_append);
-  #begin parameter testing for intein phylogeny
-  print "Beginning intein simulation parameters test.\n Testing will conclude once the user is happy with the tree and set parameters.\n"
-  tree_simulation_parameter_testing($user_check,$tree_type);
-  #save parameters settled on
-  my %extein_inputs =  %inputs;
+  #get user inputs and test until they are satisfied
+  my %extein_inputs = test_parameters("extein");
+  my %intein_inputs = test_parameters("intein");
+
   #once the user is satisfied with all parameters, ask for number of experiments to run
   print "Parameters have been set; ready to run experiment.\n How many samples would you like to create?\n\n"
   my $number_of_samples = <STDIN>;
   chomp $number_of_samples;
-  #next up loop tree creating and intein insertion process
+
+  #create the number of trees for inteins and exteins based on the number of experiments requested
+  my @extein_phylogenies = simulate_n_trees("extein",$number_of_samples,%extein_inputs);
+  my @intein_phylogenies = simulate_n_trees("intein",$number_of_samples,%intein_inputs);
+
+  #pair each extein tree with a random intein tree. Each pairing constitutes a sample
+  my %paired_intein_extein_trees;
+  foreach my $extein_tree (@extein_phylogenies){
+    my $random_intein_tree = splice(@intein_phylogenies,@intein_phylogenies[rand($intein_phylogenies)],1);
+    $paired_intein_extein_trees{$extein_tree}=$random_intein_tree;
+  }
+
+  #foreach paired sample invade the exteins with the inteins
+  invade_exteins(%paired_intein_extein_trees);
+}
+
+sub test_parameters{
+  #takes tree type as input, returns user determined inputs for that tree type.
+  my $tree_type = shift;
+  my $user_check = "N";
+  my $input_primer_append = "Please input parameters for the $tree_type phylogeny now!\n"."$input_primer";
+  %inputs = parse_and_check_inputs($input_primer_append);
+  #begin parameter testing for phylogeny of interest
+  print "Beginning extein simulation parameters test.\n Testing will conclude once the user is happy with the tree and set parameters.\n"
+  parameter_testing_loop($user_check,$tree_type);
+  #save parameters settled on
+  my %downstream_inputs =  %inputs;
+  return(%downstream_inputs)
 }
 
 sub parse_and_check_inputs{
@@ -94,54 +104,57 @@ sub parse_and_check_inputs{
   return(%inputs_for_tree);
 }
 
-sub tree_simulation_parameter_testing{
+sub parameter_testing_loop{
   #takes a y/n toggle, tree extsension and prossibly tree object as inputs
   #loops through tree building process until the user is satisfied based on the
   #global parameters
   #returns nothing, but the final chosen parameters will be in the global hash %inputs
   my $toggle = shift;
   my $tree_extension = shift;
-  my $hashref = shift || "";
   if(uc($toggle) eq "N"){
-    my $evolver_tree = tree_simulation();
-    rename($evolver_tree, "$tree_extension"."_tree.tre");
-    $evolver_tree = "$tree_extension"."_tree.tre";
+    my $evolver_tree = simulate_1_tree();
     my $pdf_tree = write_tree($evolver_tree);
     print "Does this simulated $tree_extension tree look good to you [Y/N]?\n";
     $toggle = <STDIN>;
     chomp $toggle;
     if(uc($toggle) eq "N"){
-      print "Would you like to change your tree building parameters [Y/N]?\n";
-      my $new_param_toggle = <STDIN>;
-      chomp $new_param_toggle;
-      if(uc($new_param_toggle) eq "Y"){
-        %inputs = parse_and_check_inputs($input_primer);
-      }
-      $hashref= tree_simulation_parameter_testing($toggle,$tree_extension);
+      %inputs = parse_and_check_inputs($input_primer);
+      parameter_testing_loop($toggle,$tree_extension);
     }
     elsif(uc($toggle) eq "Y"){
       return();
-    }
-    else{
-      print "$toggle is not Y or N. Please print Y or N to continue...\n";
-      $toggle = <STDIN>;
-      chomp $toggle;
-      tree_simulation_parameter_testing($toggle,$tree_extension,$tree_to_return);
     }
   }
   elsif(uc($toggle) eq "Y"){
     return();
   }
-  else{
-    print "$toggle is not Y or N. Please print Y or N to continue...\n";
-    $toggle = <STDIN>;
-    chomp $toggle;
-    $hashref = tree_simulation_parameter_testing($toggle,$tree_extension,$tree_to_return);
-  }
   return();
 }
 
-sub tree_simulation{
+sub simulate_n_trees{
+  #takes hash of evolver inputs, a file handle, and the number of simulations as input
+  #returns array of tree files
+  my $file_handle = shift;
+  my $simulation_num = shift;
+  my %inputs_for_tree = shift;
+  my @tree_files;
+  #make directory to store trees in
+  mkdir($file_handle);
+
+  #change global inputs to ones needed
+  %inputs = %inputs_for_tree;
+  my @loop = (1..$simulation_num);
+  foreach my $num (@loop){
+    my $phylogeney = simulate_1_tree();
+    my $path_to_file = "$file_handle\$file_handle\_$num\.tree";
+    copy($phylogeney,$path_to_file);
+    push(@tree_files,$path_to_file);
+    unlink $phylogeney;
+  }
+  return(@tree_files);
+}
+
+sub simulate_1_tree{
   #simulated a tree based on parameters provided
   open(my $evolver_pipe, '|-', 'paml-evolver') or die "Could not open pipe to paml-evolver: $!\n";
   print $evolver_pipe "1\n"; #uses random unrooted tree option
@@ -194,6 +207,83 @@ sub show_pdf {
   print $pdf;
 }
 
+sub invade_exteins{
+  #takes a set of paired intein and extein trees as input
+  #invades the extein trees with the inteins using a monte carlo chain
+  #returns a nested hash for which the key is the extein tree, and has
+  #data for which tips are invaded, and with which intein as nested data
+  my %tree_samples = shift;
+  foreach my $extein_tree (keys %tree_samples){
+    my($extein_tree_object,@extein_tips)=readin_newick($extein_tree);
+    my($intein_tree_object,@intein_tips)=readin_newick($tree_samples{$extein_tree});
+
+    #get pairwise distance between all nodes in intein and extein tree
+    my %extein_pairwise_distances= pairwise_patristic_distance($extein_tree_object,@extein_tips);
+    my %intein_pairwise_distances= pairwise_patristic_distance($intein_tree_object,@intein_tips);
+
+    #select a random intein and random extein to invade w/ said intein
+    my $intein_tip = @intein_tips[rand($intein_tips)];
+    my $extein_tip = @extein_tips[rand($extein_tips)];
+
+    #take the random intein/extein pair as the starting point and begin the MC chain
+    #test if there is still an intein that hasn't invaded a sequence.
+    my %paired_sequences;
+    until(!@intein_tips){
+      $paired_sequences{$extein_tip}=$intein_tip;
+      my $closest_intein = get_smallest_distance($intein_tip,%intein_pairwise_distances);
+      my %distance_from_current_extein = get_distances_from_tip($extein_tip,%extein_pairwise_distances);
+      ### Keep going here!
+
+
+
+
+
+
+
+      
+    }
+  }
+}
+
+sub get_distances_from_tip{
+  #takes a nested hash of distances and key1
+  #the hash is formatted as key1->key2->value
+  #returns a hash of key2's with the associated distance to key1
+  my $key1 = shift;
+  my %distance_hash = shift;
+  foreach my $key (keys %distance_hash){
+    next if($key ne $key1);
+    my %return_hash;
+    foreach my $key2 (keys %{$distance_hash{$key1}}){
+      next if($key2 eq $key);
+      $return_hash{$key2}=$distance_hash{$key1}->{$key2};
+    }
+  }
+  return(%return_hash);
+}
+
+sub get_smallest_distance{
+  #takes a nested hash of distances and key1
+  #the hash is formatted as key1->key2->value
+  #returns the key2 with the smallest distance to key1
+  my $key1 = shift;
+  my %distance_hash = shift;
+  foreach my $key (keys %distance_hash){
+    next if($key ne $key1);
+    my $smallest = "toggle";
+    my $key_holder;
+    foreach my $key2 (keys %{$distance_hash{$key1}}){
+      if(!$smallest="toggle"){
+        if($smallest < $distance_hash{$key1}->{$key2}){
+          next;
+        }
+      }
+      $smallest = $distance_hash{$key1}->{$key2};
+      $key_holder = $key2;
+    }
+  }
+  return($key_holder);
+}
 
 sub readin_newick{
   #readin newick format tree file
@@ -206,14 +296,6 @@ sub readin_newick{
   return($tree_object,@tree_taxa);
 }
 
-sub random_array_element{
-  #readin array
-  #return random element from said array
-  my @array = @_;
-  my $random_entry = $array[rand @array];
-  return($random_entry);
-}
-
 sub get_patristic_distance{
   #takes two tree nodes and the associated tree object as inputs
   #returns the distance between said nodes
@@ -224,23 +306,66 @@ sub get_patristic_distance{
   return($distance);
 }
 
-sub find_nearest_neighbors{
-  #takes an array of tree tip nodes, a node of interest, and a tree object as inputs
-  #returns a hash of tips paired to their distance from the node of interest
+sub pairwise_patristic_distance{
+  #takes an array of tree tip nodes, and a tree object as inputs
+  #returns a nested hash of tips paired to their distance from the node of interest
   my $tree_object = shift;
-  my $tip_of_interest = shift;
   my @array_of_tips = @_;
   my %distances;
   foreach my $tip (@array_of_tips){
-    if($tip eq $tip_of_interest){
-      next;
-    }
-    else{
-      my $node_distance = get_patristic_distance($tip,$tip_of_interest);
-      $distances{$tip}=$node_distance;
+    foreach my $tip_to_compare (@array_of_tips){
+      my $node_distance = get_patristic_distance($tip,$tip_to_compare);
+      $distances{$tip}={$tip_to_compare}=$node_distance;
     }
   }
   return(%distances);
+}
+
+sub monte_carlo_chain{
+  #takes a window width
+  #and an array with associated distances from the current position
+  #returns the new location
+  my $window_width = shift;
+  my @distance_of_tips = shift;
+  my $current_position = 0;
+
+  #uniform random deviant
+  my $uniform_random = rand();
+
+  #creates a integer value based on the window size
+  #then gets new position proposal
+  my $new_position = $current_position + int($uniform_random*$window_width);
+
+  my $acceptor = accept_or_deny_move(@distance_of_tips[$new_position]);
+
+  if($acceptor == 1){
+    return($new_position);
+  }
+  else{
+    return(0);
+  }
+}
+
+sub accept_or_deny_move{
+  #given an edge length for a move as input, find the probability of making that proposed position the new position
+  #using an exponential distribution and a uniform random deviant
+  #returns a boolean 1/0
+
+  my $lenght_to_new_position = shift;
+  #rate paramaeter for the exponential dist.
+  my $lambda = 0.5;
+
+  #prob of acceptance based on dist.
+  my $probability_of_acceptance = ($lambda*(exp(-$lambda*$lenght_to_new_position)));
+
+  my $uniform_random_deviant = rand();
+  #compare uniform random to prob of acceptance. If lower, new position is accpeted!
+  if($uniform_random_deviant <= $probability_of_acceptance){
+    return(1);
+  }
+  else{
+    return(0);
+  }
 }
 
 sub old_code{
